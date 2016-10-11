7baa29c0af28d11aec864baa4fa9fb76532c7918
Jason ZHANG <zhangqianhao1408@gmail.com>
2016-10-11 14:22:27 +0800

    sieve of eratosthenes
    a homework for my elementary number theory course.
    1 uses the basic sieve with 2 typical optimization.
    2 uses recursion instead. in my testing 2 seems to be a little bit faster than 1.

---
+++ Eratosthenes.py
@@ -0,0 +1,40 @@
+﻿# -*- coding: utf-8 -*-
+# 基于改进1与改进2
+import sys
+import time
+import math
+
+def primes(n):      # find the prime numbers in range(1,n+1)
+    n = int(n)
+    assert(n > 0)
+    if n == 1:
+        return []         
+    if n == 2:
+        return [2]
+        
+    f = [False, True] 
+    f.extend([True, False]*int(n/2-1))
+    if n % 2 == 1:   
+        f.append(True)
+    
+    p = 3
+    while p*p <= n:
+        q = p*p
+        while q <= n:
+            f[q-1] = False
+            q += p
+        p+=2
+        while (not f[p-1]) and (p*p <= n):
+            p+=2
+                         
+    P = []
+    P.append(2)
+    for i in range(3,n,2):
+        if f[i-1]:
+            P.append(i)
+    return P
+
+start = time.clock()  
+#N = sys.argv[1] 
+primes(641)
+print(primes)
---
+++ Eratosthenes2.py
@@ -0,0 +1,37 @@
+﻿# -*- coding: utf-8 -*-
+# 递归实现无改进
+import sys
+import time
+import math
+
+def primes(n):      # find the prime numbers in range(1,n+1)
+    n = int(n)
+    assert(n > 0)
+    if n == 1:
+        return []         
+    if n == 2:
+        return [2]
+        
+    f = [False, True] 
+    f.extend([True, False]*int(n/2-1))
+    if n % 2 == 1:   
+        f.append(True)
+    
+    sp = primes(int(math.sqrt(n)))
+    for each in sp[1:]:
+        base = each*each
+        while base < n:
+            f[base-1] = False
+            base += each
+
+    P = []
+    P.append(2)
+    for i in range(3,n,2):
+        if f[i-1]:
+            P.append(i)
+    return P
+
+start = time.clock()  
+#N = sys.argv[1] 
+primes(641)
+print(primes(641))
